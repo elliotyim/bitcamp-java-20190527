2b4b52ff4cd9c03965970d4b13440ef7711d1aba
yimsb01 <yimsb02@naver.com>
2019-07-08 15:42:06 +0900

    v10 백업완료

---
+++ bitcamp-java-application/v10/src/main/java/com/eomcs/lms/BoardHandler.java
@@ -0,0 +1,30 @@
+package com.eomcs.lms;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class BoardHandler {
+  static Board[] boards = new Board[100];
+  static int boardsSize = 0;
+  static Scanner keyScan;
+  
+  static void addBoard() {
+    Board board = new Board();
+    board.no = Input.getIntValue("번호? ");
+    board.contents = Input.getStringValue("내용? ");
+    board.createdDate = new Date(System.currentTimeMillis());
+    board.viewCount = 0;
+    
+    boards[boardsSize++] = board;
+    
+    System.out.println("저장하였습니다.");
+  }
+  
+  static void listBoard() {
+    for (int i = 0; i < boardsSize; i++) {
+      Board board = boards[i];
+      System.out.printf("%s, %s\t, %s, %s\n",
+          board.no, board.contents, board.createdDate, board.viewCount);
+    }
+  }
+}
---
+++ bitcamp-java-application/v10/src/main/java/com/eomcs/lms/Member.java
@@ -0,0 +1,13 @@
+package com.eomcs.lms;
+
+import java.sql.Date;
+
+public class Member {
+  int no;
+  String name;
+  String email;
+  int password;
+  String photo;
+  String phoneNum;
+  Date registeredDate;
+}
