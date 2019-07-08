80444295fc40dfb660e5a5dc3d7bf72a114706ef
yimsb01 <yimsb02@naver.com>
2019-07-08 16:22:03 +0900

    v11 백업완료

---
+++ bitcamp-java-application/v11/src/main/java/com/eomcs/lms/domain/Member.java
@@ -0,0 +1,13 @@
+package com.eomcs.lms.domain;
+
+import java.sql.Date;
+
+public class Member {
+  public int no;
+  public String name;
+  public String email;
+  public int password;
+  public String photo;
+  public String phoneNum;
+  public Date registeredDate;
+}
