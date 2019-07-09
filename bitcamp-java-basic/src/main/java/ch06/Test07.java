ebc9653d33c310de69d3a5af1c113900ec0b726f
yimsb01 <yimsb02@naver.com>
2019-07-09 16:32:06 +0900

    v12 백업완료

--- bitcamp-java-basic/src/main/java/ch06/Test07.java
+++ bitcamp-java-basic/src/main/java/ch06/Test07.java
@@ -5,9 +5,11 @@ public class Test07 {
   public static void main(String[] args) {
     // 가변 파라미터는 호출하는 쪽에서 배열을 만들 필요가 없다.
     // 자바가 자동으로 배열을 만든다.
-    plus1();
+    plus1(); // => plus1(new int[] {}); //컴파일러가 이렇게 자동으로 바꿔서 주소값을 리턴함
     plus1(10);
     plus1(10, 20);
+    
+    //plus2(10); //컴파일 오류
     
     // 배열 레퍼런스는 호출자가 만들어서 넘겨야 한다. 그래서 호출할 때는 가변 파라미터가 편하다.
     plus2(new int[] {});
