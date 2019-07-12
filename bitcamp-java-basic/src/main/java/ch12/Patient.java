0c1743ecb615716bb71f1ec2e3e06d6370d40c21
yimsb01 <yimsb02@naver.com>
2019-07-12 11:36:00 +0900

    v15 백업완료

---
+++ bitcamp-java-basic/src/main/java/ch12/Patient.java
@@ -0,0 +1,17 @@
+package ch12;
+
+public class Patient {
+  public static final int WOMAN = 1;
+  public static final int MAN = 2;
+  
+  String name;
+  int age;
+  int height;
+  int weight;
+  int gender;
+  
+  public String toString() {
+    return String.format("name=%s, age=%s, height=%s, weight=%s, gender=%s",
+        this.name, this.age, this.height, this.weight, this.gender);
+  }
+}
