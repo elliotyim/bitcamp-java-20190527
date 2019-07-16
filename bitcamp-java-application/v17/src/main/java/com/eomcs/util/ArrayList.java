62fea0f4eb3ebce731eb6ae9299a707c297e80c3
yimsb01 <yimsb02@naver.com>
2019-07-16 09:47:44 +0900

    v17 수업내용 백업

---
+++ bitcamp-java-application/v17/src/main/java/com/eomcs/util/ArrayList.java
@@ -0,0 +1,37 @@
+package com.eomcs.util;
+
+public class ArrayList {
+  private static final int DEFAULT_CAPACITY = 100;
+  
+  private Object[] list;
+  private int size = 0;
+  
+  public ArrayList() {
+    // 특수 문법. 생성자를 호출함
+    // 다만 생성자만이 다른 생성자를 호출함.
+    this(DEFAULT_CAPACITY); //생성자에서 다른 생성자를 호출할 수 있다.
+  }
+  
+  public ArrayList(int initialCapacity) {
+    if (initialCapacity < 50 || initialCapacity > 10000) {
+      list = new Object[DEFAULT_CAPACITY];
+    }else {
+      list = new Object[initialCapacity];
+    }
+  }
+  
+  public void add(Object obj) {
+    if (this.size == list.length) {
+      throw new RuntimeException("배열이 꽉 찼습니다!");
+    }
+    //this(100); // 컴파일 오류! 일반 메서드는 생성자를 호출할 수 없다.
+    this.list[this.size++] = obj;
+  }
+  public Object[] toArray() {
+    Object[] arr = new Object[this.size];
+    for (int i = 0; i < this.size; i++) {
+      arr[i] = this.list[i];
+    }
+    return arr;
+  }
+}
