cd8faba421051595f8e77544e801a2018f81e28d
yimsb01 <yimsb02@naver.com>
2019-08-06 14:34:27 +0900

    수업내용 백업

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/client/MemberDaoProxy.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/client/MemberDaoProxy.java
@@ -2,78 +2,100 @@ package com.eomcs.lms.client;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.List;
 
 import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
 
 public class MemberDaoProxy implements MemberDao {
-  
-  ObjectInputStream in;
-  ObjectOutputStream out;
-  
-  public MemberDaoProxy(ObjectInputStream in, ObjectOutputStream out) {
-    this.in = in;
-    this.out = out;
+
+  String host;
+  int port;
+
+  public MemberDaoProxy(String host, int port) {
+    this.host = host;
+    this.port = port;
   }
+
   @Override
   public int insert(Member member) throws Exception {
-    out.writeUTF("/member/add");
-    out.writeObject(member);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try(Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+      out.writeUTF("/member/add");
+      out.writeObject(member);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
   @SuppressWarnings("unchecked")
   @Override
   public List<Member> findAll() throws Exception {
-    out.writeUTF("/member/list");
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return (List<Member>)in.readObject();
+    try(Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+      out.writeUTF("/member/list");
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return (List<Member>)in.readObject();
+    }
   }
-  
+
   @Override
   public Member findBy(int no) throws Exception {
-    out.writeUTF("/member/detail");
-    out.writeInt(no);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return (Member)in.readObject();
+    try(Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+      out.writeUTF("/member/detail");
+      out.writeInt(no);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return (Member)in.readObject();
+    }
   }
-  
+
   @Override
   public int update(Member member) throws Exception {
-    out.writeUTF("/member/update");
-    out.writeObject(member);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try(Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+      out.writeUTF("/member/update");
+      out.writeObject(member);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
   @Override
   public int delete(int no) throws Exception {
-    out.writeUTF("/member/delete");
-    out.writeInt(no);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try(Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+      out.writeUTF("/member/delete");
+      out.writeInt(no);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
 }
