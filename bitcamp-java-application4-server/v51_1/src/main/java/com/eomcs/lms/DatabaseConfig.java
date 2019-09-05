0fa957abb10c55551c9527a23c11f26a413f9271
yimsb01 <yimsb02@naver.com>
2019-09-05 16:45:37 +0900

    수업내용 백업

---
+++ bitcamp-java-application4-server/v51_1/src/main/java/com/eomcs/lms/DatabaseConfig.java
@@ -0,0 +1,50 @@
+package com.eomcs.lms;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@PropertySource("classpath:com/eomcs/lms/conf/jdbc.properties")
+public class DatabaseConfig {
+  
+  @Value("${jdbc.driver}") String jdbcDriver;
+
+  @Value("${jdbc.url}") String jdbcUrl;
+  
+  @Value("${jdbc.username}") String jdbcUsername;
+  
+  @Value("${jdbc.password}") String jdbcPassword;
+  
+  @Bean
+  public DataSource dataSource() {
+    System.out.println("dataSource()");
+    // spring-jdbc 라이브러리에서 제공해주는 클래스를 사용하여 DataSource 구현체 만들기
+    DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(this.jdbcDriver); 
+    ds.setUrl(this.jdbcUrl);
+    ds.setUsername(this.jdbcUsername);
+    ds.setPassword(this.jdbcPassword);
+    return ds;
+  }
+  
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    System.out.println("transactionManager()");
+    // 트랜잭션 관리자를 생성할 때 DataSource(DB 커넥션풀) 구현체를 요구한다.
+    // Spring IoC 컨테이너에 들어 있는 것을 꺼내기 위해 
+    // 파라미터로 선언하라.
+    return new DataSourceTransactionManager(dataSource);
+  }
+}
+
+
+
+
+
+
